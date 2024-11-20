72073034cb08f7bfe7f2a0a7cadf29540fe19466
Mitchel0n <mitchel.bustamante0123@gmail.com>
2024-11-20 15:56:32 -0300

    Login

--- LoginPoleras/appPoleras/views.py
+++ LoginPoleras/appPoleras/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+@login_required
+def Home(request):
+    return render(request, 'index.html')
+    
+def LoginView(request):
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
+            return redirect('home')
+        
+        else:
+            messages.error(request, "Invalid login credentials")
+            return redirect('login')
+
+    return render(request, 'login.html')
